56b9d78a0eb089a04ac458782eb4581de283a2ef
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-03-06 15:27:40 -0500

    added some variables to script.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-console.log("akan-names");
+console.log(Akan-names)
 //formula to determine day of month
 
 //creating arrays for female and male Akan names and days of the week
-  let days0fWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  let maleAkanNames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-  let femaleAkanNames=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
-  //for,ula to determine day of birth
+  var days0fWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  var maleAkanNames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+  var femaleAkanNames=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+  let form=document.getElementById("form");
+  form.addEventListener("Submit", (Event))=>{event.preventDefault();
+    let gender=document.getElementsByName("gender")
 
 
