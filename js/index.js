a110a8f49f794d0457307395b5abdd005c3e75df
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-03-06 13:43:16 -0500

    added a comment

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 console.log("akan-names");
+//creating arrays for female and male Akan names and days of the week
   let days0fWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   let maleAkanNames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
   let femaleAkanNames=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
