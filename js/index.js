8c14eb738bbd4a6dc2feac9d6cf3bc290d6b2cf8
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-03-06 18:26:05 -0500

    added a form validation

--- js/index.js
+++ js/index.js
@@ -1,33 +1,34 @@
-console.log(Akan-names)
-let form= document.getElementById("form");
-  form.addEventListener("Submit", (e)) ; {e.preventDefault();
+console.log(Akan - names)
+let form = document.getElementById("form");
+form.addEventListener("Submit", (e)); {
+    e.preventDefault();
 }
 //formula to determine day of month
 
 //creating arrays for female and male Akan names and days of the week
-  var days0fWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  var maleAkanNames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-  var femaleAkanNames=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+var days0fWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var maleAkanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+var femaleAkanNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
 gender = document.getElementById("Gender").value
-    date= document.getElementById("date").value
+date = document.getElementById("date").value
 
-    let yearOfBirth=date.getFullYear();
-    let monthOfTheYear=date.getMonth();
-    let day=daysOfWeek[date.getDay()];
+let yearOfBirth = date.getFullYear();
+let monthOfTheYear = date.getMonth();
+let day = daysOfWeek[date.getDay()];
 //formula to get daysOfWeek
 
- days0fWeek= Math.floor(((centuryOfBirth.slice(0,2)-2*centuryOfBirth.slice(0,2-1)+((5*yearOfBirth(2,4)/4)+((26*monthOfBirth+1)/10))+dayOfBirth)%7);
+days0fWeek = Math.floor(((centuryOfBirth.slice(0, 2) - 2 * centuryOfBirth.slice(0, 2 - 1) + ((5 * yearOfBirth(2, 4) / 4) + ((26 * monthOfTheYear + 1) / 10)) + dayOfBirth) % 7);
 
- let dayOfBirth;
- 
- if (daysOfWeek == 0 && gender == "female") {
-    dayOfBirth = "Saturday";
-    akanName = femaleAkanNames[0];
-} else if (daysOfWeek == 0 && gender == "male") {
-    dayOfBirth = "Saturday";
-    akanName = maleAkanNames[0];
-}
+function dayValidation() {
+
+    if (daysOfWeek == 0 && gender == "female") {
+        dayOfBirth = "Saturday";
+        akanName = femaleAkanNames[0];
+    } else if (daysOfWeek == 0 && gender == "male") {
+        dayOfBirth = "Saturday";
+        akanName = maleAkanNames[0];
+    }
 
     if (daysOfWeek == 1 && gender == "female") {
         dayOfBirth = "Sunday";
@@ -72,9 +73,10 @@ gender = document.getElementById("Gender").value
         dayOfBirth = "Friday";
         akanName = maleAkanNames[6];
     }
-    let result = document.getElementById("reveal-result").innerHTML = "You were born on a " + dayOfBirth + " so your akan name is " + akanName + "!"
-    document.getElementById("form").reset();
-});
+}
+let result = document.getElementById("reveal-result").innerHTML = "You were born on a " + dayOfBirth + " so your akan name is " + akanName + "!"
+document.getElementById("form").reset();
+
 var days0fWeek = new Date();
 
 console.log(days0fWeek.getDay()); // sunday = 0, monday = 1, etc..
