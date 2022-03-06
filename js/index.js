6b4e8e62a1ae497d06198858dc74cd72c3688309
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-03-06 14:18:14 -0500

    added more descriptions

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
 console.log("akan-names");
+//formula to determine day of month
+
 //creating arrays for female and male Akan names and days of the week
   let days0fWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   let maleAkanNames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
   let femaleAkanNames=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
-let button=document.getElementById("button");
-button.addEventListener("click",myFunction);
-function myFunction(){
-    let date=document.getElementById("date").ariaValueMax
-    alert(date)
-}
+  //for,ula to determine day of birth
 
 
