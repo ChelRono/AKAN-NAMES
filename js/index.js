6a3c70e0dc663b813463b1b463f87b5b293721b7
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-03-06 13:40:53 -0500

    added names to script.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 console.log("akan-names");
+  let days0fWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  let maleAkanNames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+  let femaleAkanNames=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 let button=document.getElementById("button");
 button.addEventListener("click",myFunction);
 function myFunction(){
